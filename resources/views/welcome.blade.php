91cccfac9317e66112c7f6d7a3694cc0ccacd86b
KusalDay <106142440+KusalDay@users.noreply.github.com>
2022-07-06 09:54:43 +0530

    .

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Caller Center</title>
+        <title>Caller Center One</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
